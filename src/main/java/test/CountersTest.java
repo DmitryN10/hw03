709ed31a9637995f55bc0d5cd25b9b85b5421cf7
Dmitry <dmitryn10@mail.ru>
2018-11-24 12:56:06 +0300

    Switched to busy spin instead of sleep

--- src/main/java/test/CountersTest.java
+++ src/main/java/test/CountersTest.java
@@ -4,6 +4,7 @@ import counters.AtomicLongCounter;
 import counters.SemaphoreCounter;
 import counters.SynchronizedCounter;
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
@@ -15,7 +16,7 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.openjdk.jmh.annotations.Mode.Throughput;
 
 public class CountersTest{
-    private static final int THREAD_COUNT = 8;
+    private static final int THREAD_COUNT = 1;
     private static final long THREAD_TIME_MS = 2;
     private static final int WARMUP_ITER = 10;
     private static final int MEASUREMENT_ITER = 100;
@@ -30,27 +31,30 @@ public class CountersTest{
     @BenchmarkMode(Throughput)
     @OutputTimeUnit(SECONDS)
     @Group("AtomicLongCounter")
-    public void testAtomicLongCounter(CounterPerfState state) {
+    public void testAtomicLongCounter(CounterPerfState state, Blackhole blackhole) {
         state.atomicLongCounter.incrementAndGet();
-        threadSleep();
+//        threadSleep();
+        busySpin(blackhole);
     }
 
     @Benchmark
     @BenchmarkMode(Throughput)
     @OutputTimeUnit(SECONDS)
     @Group("SemaphoreCounter")
-    public void testSemaphoreCounter(CounterPerfState state) {
+    public void testSemaphoreCounter(CounterPerfState state, Blackhole blackhole) {
         state.semaphoreCounter.incrementAndGet();
-        threadSleep();
+//        threadSleep();
+        busySpin(blackhole);
     }
 
     @Benchmark
     @BenchmarkMode(Throughput)
     @OutputTimeUnit(SECONDS)
     @Group("SynchronizedCounter")
-    public void testSynchronizedCounter(CounterPerfState state) {
+    public void testSynchronizedCounter(CounterPerfState state, Blackhole blackhole) {
         state.synchronizedCounter.incrementAndGet();
-        threadSleep();
+//        threadSleep();
+        busySpin(blackhole);
     }
 
     public static void main(String[] args) throws RunnerException {
@@ -70,5 +74,12 @@ public class CountersTest{
         try {
             sleep(CountersTest.THREAD_TIME_MS);
         } catch (InterruptedException e) { }
+    }
+
+    private final static int BUSY_SPIN_ITERATION_AMOUNT = 100000;
+    private void busySpin(Blackhole blackhole) {
+        for(int i = 0; i < BUSY_SPIN_ITERATION_AMOUNT; i++){
+            blackhole.consume(i);
+        }
     }
 }
